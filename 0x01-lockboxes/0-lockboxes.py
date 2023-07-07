8f184dc7b62ec75ef3cbf29c3930499e02432661
chiedozie013 <chiedozie013@gmail.com>
2023-07-07 00:52:46 +0000

    lockboxes task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Solves the lock boxes challenge
+"""
+
+
+def canUnlockAll(boxes):
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
